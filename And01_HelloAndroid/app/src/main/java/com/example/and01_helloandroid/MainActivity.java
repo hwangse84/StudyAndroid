e57d5b32221f82fac964d48b96d1452d987252e0
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-11-13 13:05:49 +0900

    android

---
+++ And01_HelloAndroid/app/src/main/java/com/example/and01_helloandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.and01_helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
